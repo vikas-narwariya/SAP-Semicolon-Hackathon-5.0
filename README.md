64383a9114d809815ff1d83610a94e367d19a470
sneha <72269836+sneha-del@users.noreply.github.com>
2021-10-31 15:18:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ApnaKhaata
---
+++ app/src/main/java/com/test/creditinfo/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.test.creditinfo;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.test.creditinfo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+
+                        WelcomeActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, 5000);
+
+    }
+}
